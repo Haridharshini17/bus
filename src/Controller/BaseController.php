5ecbea46a3fadcd20c9149a4a28fb6b556e40f74
Asplap3159 <haridha.senthil@aspiresys.com>
2023-06-29 23:54:58 +0530

    Added changes in Base files

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -11,30 +11,49 @@ use Doctrine\Persistence\ManagerRegistry;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface as AuthEncoder;
 use App\Service\BusService;
 use App\Service\AuthService;
+use App\Form\Type\AuthForm;
 
 class BaseController extends AbstractController
 {
-    public function __construct(
-        AuthEncoder $encoder, 
-        ManagerRegistry $doctrine, 
-        EntityManagerInterface $entityManager, 
-        BusService $busService,
-        AuthService $authService
-    ) {
+    public function __construct(AuthEncoder $encoder, BusService $busService, AuthService $authService) 
+    {
         $this->authEncoder = $encoder;
-        $this->db = $doctrine->getManager();
-        $this->doctrine = $doctrine;
         $this->busService = $busService;
         $this->authService = $authService;
     }
 
-    /**
-     * Method for common insert process.
-     */
-    public function dbInsert($user)
+    public function getAuthFormDetails($class, $data, $request)
     {
-        $entityManager = $this->doctrine->getManager();
-        $entityManager->persist($user);
-        $entityManager->flush($user);
+        $createForm = $this->createForm($class, $data);
+        $user = $this->authService->getFormDetails($request, $createForm);
+        return $user;
     }
+
+    public function getBusFormDetails($class, $data, $request)
+    {
+        $createForm = $this->createForm($class, $data);
+        $user = $this->busService->getFormDetails($request, $createForm);
+        return $user;
+    }
+
+    protected function renderResponse($info, $statusCode = Response::HTTP_OK)
+    {
+        if ($statusCode >= Response::HTTP_BAD_REQUEST) {
+            $this->reportError($info, $statusCode);
+        }
+        
+        return new Response($info, $statusCode);
+    }
+
+    /**
+     *
+     * @param type $statusInfo
+     * @param type $statusCode
+     *
+     */
+    protected function reportError($statusInfo, $statusCode = Response::HTTP_BAD_REQUEST)
+    {
+        return new Response($statusInfo, $statusCode);
+    }
+
 }
